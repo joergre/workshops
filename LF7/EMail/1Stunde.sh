83c7a773ab37a0b18bf4f64213c96437c5d960c7
Joerg Reuter <joerg@reuter.sc>
2016-06-16 13:31:42 +0200

    vMail

--- LF7/EMail/1Stunde.sh
+++ LF7/EMail/1Stunde.sh
@@ -1,9 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os, subprocess
+import sys, traceback
+import crypt
+# .my.cnf kann das Passwort und Benutzername von MySQL-Nutzern hinterlegt werden. Achtung: Sicherheitsrisiko!
 cp my.cnf ~/.my.cnf 
-apt update && apt -y upgrade
+# Update Upgrade
+cmd1 = os.system("apt-get update > vb.log && sudo apt-get upgrade -y >> vb.log")
 # Passwort und Benutzer für mysql festlegen:
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password fbs'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password fbs'
-apt -y install mysql 
-mysql -u root -pfbs -e "create database vmail;"
-mysql -u root -pfbs -e "GRANT ALL ON vmail.* TO 'vmail'@'localhost' IDENTIFIED BY 'fbs';"
-mysql "vmail" < "mysql"
+cmd1 = os.system("debconf-set-selections <<< 'mysql-server mysql-server/root_password password fbs'")
+cmd1 = os.system("debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password fbs'")
+# Datenbank installieren
+cmd1 = os.system("apt -y install mysql") 
+# Datenban vmail anlegen
+cmd1 = os.system("mysql -u root -pfbs -e 'create database vmail;'")
+# benutzer vmail anlegen
+cmd1 = os.system("mysql -u root -pfbs -e 'GRANT ALL ON vmail.* TO 'vmail'@'localhost' IDENTIFIED BY 'fbs';'")
+# Tabellen anlegen
+cmd1 = os.system("mysql 'vmail' < 'mysql')
--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 git config --global user.email "joerg@reuter.sc"
 git config --global user.name "Joerg Reuter"
 git add --all
-git commit -m 'md5-Coll'
+git commit -m 'vMail'
 git push
